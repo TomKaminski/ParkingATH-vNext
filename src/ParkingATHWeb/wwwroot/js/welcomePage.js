37f270989165cb3d871d63c5552aeb22872fa8e6
Tomasz Kamiński <tkaminski93@gmail.com>
2016-01-22 17:16:42 +0100

    welcome page design

--- src/ParkingATHWeb/wwwroot/js/welcomePage.js
+++ src/ParkingATHWeb/wwwroot/js/welcomePage.js
@@ -1,6 +1,75 @@
 ﻿(function ($) {
-    $(function () {
-        $('.button-collapse').sideNav();
-        $('.parallax').parallax();
+
+
+    $(window).load(function () {
+        $('#status').fadeOut();
+        $('#preloader').delay(300).fadeOut('slow');
     });
+
+    $(document).ready(function () {
+
+        /* ---------------------------------------------- /*
+		 * Smooth scroll / Scroll To Top
+		/* ---------------------------------------------- */
+
+        $('a[href*=#]').bind("click", function (e) {
+
+            var anchor = $(this);
+            $('html, body').stop().animate({
+                scrollTop: $(anchor.attr('href')).offset().top - 65
+            }, 1000);
+            e.preventDefault();
+        });
+
+        $(window).scroll(function () {
+            if ($(this).scrollTop() > 100) {
+                $('.scroll-up').fadeIn();
+            } else {
+                $('.scroll-up').fadeOut();
+            }
+        });
+
+        /* ---------------------------------------------- /*
+		 * Navbar
+		/* ---------------------------------------------- */
+
+        $('.header').sticky({
+            topSpacing: 0
+        });
+
+        $('body').scrollspy({
+            target: '.navbar-custom',
+            offset: 70
+        });
+
+
+        /* ---------------------------------------------- /*
+		 * Home BG
+		/* ---------------------------------------------- */
+
+        $(".screen-height").height($(window).height());
+        $(".screen-height-60").height(($(window).height()) * 0.6);
+
+        $(window).resize(function () {
+            $(".screen-height").height($(window).height());
+            $(".screen-height-60").height(($(window).height()) * 0.6);
+        });
+
+        if (/Android|webOS|iPhone|iPad|iPod|BlackBerry/i.test(navigator.userAgent)) {
+            $('#home').css({ 'background-attachment': 'scroll' });
+        } else {
+            $('#home').parallax('50%', 0.1);
+        }
+
+
+        /* ---------------------------------------------- /*
+		 * WOW Animation When You Scroll
+		/* ---------------------------------------------- */
+
+        wow = new WOW({
+            mobile: false
+        });
+        wow.init();
+    });
+
 })(jQuery);
