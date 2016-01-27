c9ad1d2ed414702e9def9a95475b9fe41fbaa0b4
Tomasz Kamiński <tkaminski93@gmail.com>
2016-01-27 07:32:05 +0100

    smart ajax form

--- src/ParkingATHWeb/wwwroot/js/Portal/Account/LoginRegisterForgot.js
+++ src/ParkingATHWeb/wwwroot/js/Portal/Account/LoginRegisterForgot.js
@@ -1,102 +1,31 @@
-﻿// 0 - LOGIN
-// 1 - register
-// 2 - forgot
-
-function showNotifications(model) {
-    for (var i = 0; i < model.SuccessNotifications.length; i++) {
-        var $toastContent = $('<span>' + model.SuccessNotifications[i] + '</span>');
-        Materialize.toast($toastContent, 8000, 'toast-green');
-    }
-    for (var j = 0; j < model.ValidationErrors.length; j++) {
-        var $toastContentError = $('<span>' + model.ValidationErrors[j] + '</span>');
-        Materialize.toast($toastContentError, 8000);
-    }
-}
-
-function setStatus() {
-    var attrData = getCurrentStatus();
+﻿function setStatus() {
+    var attrData = getCurrentForm();
     var windowWidth = $(window).width();
-    if (windowWidth > 992 && attrData === 0) {
+    if (windowWidth > 992 && attrData === "login-form") {
         $('.card-content.switcher-box').css("margin-left", "50%");
-    } else if (windowWidth <= 992 && attrData === 0) {
+    } else if (windowWidth <= 992 && attrData === "login-form") {
         $('.card-content.switcher-box').css("margin-left", "0%");
     }
 }
 
-function getCurrentStatus() {
-    return parseInt($('#switcher-box').attr('data-status'));
+function getCurrentForm() {
+    return $('#switcher-box').attr('data-status');
 }
 
-function clearFormErrors() {
-    $('#register-form, #login-form, #forgot-form').each(function () {
-        $(this).find('input[type=text], input[type=password]').each(function () {
-            $(this).removeClass('input-validation-error').removeClass('valid');
-            $(this).val(null);
-        });
-        $(this).find('span.field-validation-error').each(function () {
-            $(this).remove();
-        });
-        $(this).find('label').each(function () {
-            $(this).removeClass('active');
-        });
-    });
-}
-
-function clearValues() {
-    $('#register-form, #login-form, #forgot-form').each(function () {
-        $(this).find('input[type=text], input[type=password]').each(function () {
-            $(this).val(null);
-        });
-        $(this).find('label').each(function () {
-            $(this).removeClass('active');
-        });
-    });
-}
-
-function setupBeforeSubmit() {
-    $('.preloader-wrapper').show();
-    resizeContainer();
-    $('.smart-move-btn, .smart-form-btn').addClass('disabled');
-}
-
-function setupAfterSubmit() {
-    $('.preloader-wrapper').hide();
-    $('.smart-move-btn, .smart-form-btn').removeClass('disabled');
-}
-
-function beforeSubmit() {
-    setupBeforeSubmit();
-}
-
-function afterSuccess(data) {
-    if (data.IsValid) {
-        clearValues();
-    }
-    setupAfterSubmit(data);
-    showNotifications(data);
+function setCurrentForm(formId) {
+    $('#switcher-box').attr('data-current-form', formId);
 }
 
 function afterSuccessLogin(data) {
     if (data.IsValid) {
-        clearValues();
+        smartAjax.clearValues();
     }
-    setupAfterSubmit(data);
+    smartAjax.setupAfterSubmit(data);
     if (data.IsValid === true) {
         window.location.replace(data.ReturnUrl);
     } else {
-        showNotifications(data);
+        smartAjax.showNotifications(data);
     }
-}
-
-function onError() {
-    $('.preloader-wrapper').hide();
-    $('.smart-move-btn, .smart-form-btn').removeClass('disabled');
-    var $toastContentError = $('<span>Wystąpił błąd serwera - spróbuj później.</span>');
-    Materialize.toast($toastContentError, 8000);
-}
-
-function checkIfNotDisabled(button) {
-    return $(button).hasClass('disabled') ? false : true;
 }
 
 function resizeContainer() {
@@ -104,40 +33,31 @@ function resizeContainer() {
 }
 
 $(document).ready(function () {
-    $('.preloader-wrapper').hide();
     resizeContainer();
     $(window).resize(function () {
         resizeContainer();
     });
 
     $('#moveRegister, #moveRegister2').click(function () {
-        clearFormErrors();
-        if (checkIfNotDisabled(this)) {
-            $('#switcher-box').attr('data-status', 1);
+        smartAjax.clearFormErrors(getCurrentForm());
+        if (smartAjax.checkIfNotDisabled(this)) {
+            setCurrentForm("register-form");
+            smartAjax.setCurrentForm(getCurrentForm());
             $('.card-content.switcher-box').animate({
-                'marginLeft': "0" 
+                'marginLeft': "0"
             });
 
             $('.inner-box').animate({
-                'marginLeft': "100%" 
+                'marginLeft': "100%"
             });
-        }
-    });
-
-    $('.smart-form-btn').click(function (e) {
-        e.preventDefault();
-        if ($(this).hasClass('disabled')) {
-            return;
-        } else {
-            var formId = $(this).data('form-id');
-            $('#'+formId).submit();
         }
     });
 
     $('#moveLogin, #moveLogin2').click(function () {
-        clearFormErrors();
-        if (checkIfNotDisabled(this)) {
-            $('#switcher-box').attr('data-status', 0);
+        smartAjax.clearFormErrors(getCurrentForm());
+        if (smartAjax.checkIfNotDisabled(this)) {
+            setCurrentForm("login-form");
+            smartAjax.setCurrentForm(getCurrentForm());
             if ($(window).width() <= 992) {
                 $('.card-content.switcher-box').animate({
                     'marginLeft': "0%"
@@ -147,7 +67,6 @@ $(document).ready(function () {
                     'marginLeft': "50%"
                 });
             }
-
             $('.inner-box').animate({
                 'marginLeft': "0"
             });
@@ -155,21 +74,21 @@ $(document).ready(function () {
     });
 
     $('#moveForgot').click(function () {
-        clearFormErrors();
-        if (checkIfNotDisabled(this)) {
-            $('#switcher-box').attr('data-status', 2);
+        smartAjax.clearFormErrors(getCurrentForm());
+        if (smartAjax.checkIfNotDisabled(this)) {
+            setCurrentForm("forgot-form");
+            smartAjax.setCurrentForm(getCurrentForm());
             if ($(window).width() <= 992) {
                 $('.card-content.switcher-box').animate({
-                    'marginLeft': "0" 
+                    'marginLeft': "0"
                 });
             } else {
                 $('.card-content.switcher-box').animate({
-                    'marginLeft': "50%" 
+                    'marginLeft': "50%"
                 });
             }
-
             $('.inner-box').animate({
-                'marginLeft': "-100%" 
+                'marginLeft': "-100%"
             });
         }
     });
@@ -179,21 +98,12 @@ $(document).ready(function () {
         setStatus();
     });
 
-    var options = {
-        beforeSubmit: beforeSubmit,
-        success: afterSuccess,
-        error: onError
-    }
-
-    $('#register-form').ajaxForm(options);
+    $('#register-form').ajaxForm(smartAjax.defaultOptions);
+    $('#forgot-form').ajaxForm(smartAjax.defaultOptions);
 
     $('#login-form').ajaxForm({
-        beforeSubmit: beforeSubmit,
+        beforeSubmit: smartAjax.beforeSubmit,
         success: afterSuccessLogin,
-        error: onError
+        error: smartAjax.onError
     });
-
-    $('#forgot-form').ajaxForm(options);
-
-
 });
---
+++ src/ParkingATHWeb/wwwroot/js/smartAjaxForms.js
@@ -0,0 +1,115 @@
+﻿var smartAjax = function () {
+    var currentForm;
+
+    var setCurrentForm = function (id) {
+        currentForm = id;
+    }
+
+    var showNotifications = function (model) {
+        for (var i = 0; i < model.SuccessNotifications.length; i++) {
+            var $toastContent = $('<span>' + model.SuccessNotifications[i] + '</span>');
+            Materialize.toast($toastContent, 8000, 'toast-green');
+        }
+        for (var j = 0; j < model.ValidationErrors.length; j++) {
+            var $toastContentError = $('<span>' + model.ValidationErrors[j] + '</span>');
+            Materialize.toast($toastContentError, 8000);
+        }
+    }
+
+    var clearFormErrors = function () {
+        $('#' + currentForm).each(function () {
+            $(this).find('input[type=text], input[type=password]').each(function () {
+                $(this).removeClass('input-validation-error').removeClass('valid');
+                $(this).val(null);
+            });
+            $(this).find('span.field-validation-error').each(function () {
+                $(this).remove();
+            });
+            $(this).find('label').each(function () {
+                $(this).removeClass('active');
+            });
+        });
+    }
+
+    var clearValues = function () {
+        $('#' + currentForm).each(function () {
+            $(this).find('input[type=text], input[type=password]').each(function () {
+                $(this).val(null);
+            });
+            $(this).find('label').each(function () {
+                $(this).removeClass('active');
+            });
+        });
+    }
+
+    function setupBeforeSubmit() {
+        $('#' + currentForm + ' .preloader-wrapper').show();
+        $('#' + currentForm + ' .smart-move-btn,' + '#' + currentForm + ' .smart-form-btn').addClass('disabled');
+    }
+
+    var setupAfterSubmit = function () {
+        $('#' + currentForm + ' .preloader-wrapper').hide();
+        $('#' + currentForm + ' .smart-move-btn,' + '#' + currentForm + ' .smart-form-btn').removeClass('disabled');
+    }
+
+    var beforeSubmit = function () {
+        setupBeforeSubmit();
+    }
+
+    var afterSuccess = function (data) {
+        if (data.IsValid) {
+            clearValues();
+        }
+        setupAfterSubmit();
+        showNotifications(data);
+    }
+
+    var onError = function () {
+        setupAfterSubmit();
+        var $toastContentError = $('<span>Wystąpił błąd serwera - spróbuj później.</span>');
+        Materialize.toast($toastContentError, 8000);
+    }
+
+    var defaultOptions = {
+        beforeSubmit: beforeSubmit,
+        success: afterSuccess,
+        error: onError
+    }
+
+    var checkIfNotDisabled = function (button) {
+        return $(button).hasClass('disabled') ? false : true;
+    }
+
+    return {
+        defaultOptions: defaultOptions,
+        onError: onError,
+        afterSuccess: afterSuccess,
+        beforeSubmit: beforeSubmit,
+        clearValues: clearValues,
+        clearFormErrors: clearFormErrors,
+        showNotifications: showNotifications,
+        setCurrentForm: setCurrentForm,
+        checkIfNotDisabled: checkIfNotDisabled,
+        setupAfterSubmit: setupAfterSubmit
+    }
+}();
+
+$(document).ready(function () {
+    $('.preloader-wrapper').hide();
+
+    $('.smart-form-btn').click(function (e) {
+        e.preventDefault();
+        if ($(this).hasClass('disabled')) {
+            return;
+        } else {
+            var formId = $(this).data('form-id');
+            smartAjax.setCurrentForm(formId);
+            $('#' + formId).submit();
+        }
+    });
+
+    $('.smart-move-btn').click(function (e) {
+        if (!smartAjax.checkIfNotDisabled(this))
+            e.preventDefault();
+    });
+});
