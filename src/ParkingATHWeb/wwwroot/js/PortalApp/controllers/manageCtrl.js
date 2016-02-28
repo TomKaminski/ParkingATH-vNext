bd6dbf4274dfb1ee3e64a5ae619c10447637e0d8
Tomasz Kamiński <tkaminski93@gmail.com>
2016-02-28 16:47:15 +0100

    settings frontend

--- src/ParkingATHWeb/wwwroot/js/PortalApp/controllers/manageCtrl.js
+++ src/ParkingATHWeb/wwwroot/js/PortalApp/controllers/manageCtrl.js
@@ -4,6 +4,39 @@
     function manageController(breadcrumbService) {
         var self = this;
         breadcrumbService.setOuterBreadcrumb('account');
+
+        self.sendChargesModel = {
+            receiverEmail: "",
+            amountOfCharges: null,
+            password: "",
+            disableButton: false
+        }
+
+        self.changePasswordModel = {
+            oldPassword: "",
+            newPassword: "",
+            repeatNewPassword: "",
+            disableButton: false
+        }
+
+        self.changeEmailModel = {
+            newEmail: "",
+            newEmailRepeat: "",
+            confirmPassword: "",
+            disableButton: false
+        }
+
+        self.changeInfoModel = {
+            firstName: "Tomasz",
+            lastName: "Kamiński",
+            disableButton: false
+        }
+
+        self.activeTab = 1;
+
+        self.setActiveTab = function(tabNumber) {
+            self.activeTab = tabNumber;
+        }
     }
 
     angular.module('portalApp').controller('manageCtrl', ['breadcrumbService', manageController]);
