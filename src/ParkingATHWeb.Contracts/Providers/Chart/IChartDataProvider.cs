c76bde1d3ce9d1b7c9f0b36566298adea99a2b6b
Tomasz Kamiński <tkaminski93@gmail.com>
2016-03-08 21:26:15 +0100

    missing files

---
+++ src/ParkingATHWeb.Contracts/Providers/Chart/IChartDataProvider.cs
@@ -0,0 +1,10 @@
+﻿using System.Threading.Tasks;
+using ParkingATHWeb.Contracts.DTO.Chart;
+
+namespace ParkingATHWeb.Contracts.Providers.Chart
+{
+    public interface IChartDataProvider
+    {
+        Task<ChartListDto> GetChartData(ChartRequestDto request);
+    }
+}
---
+++ src/ParkingATHWeb/Areas/Portal/ViewModels/Chart/ChartDataReturnModel.cs
@@ -0,0 +1,13 @@
+﻿using ParkingATHWeb.Shared.Enums;
+
+namespace ParkingATHWeb.Areas.Portal.ViewModels.Chart
+{
+    public class ChartDataReturnModel
+    {
+        public string[] Labels { get; set; }
+        public int[] Data { get; set; }
+        public ChartType Type { get; set; }
+        public ChartGranuality Granuality { get; set; }
+        public int UserId { get; set; }
+    }
+}
