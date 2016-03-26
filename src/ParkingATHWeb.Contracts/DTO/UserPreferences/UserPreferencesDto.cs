cd35a32246b8e7255515d25868a160b8ccb8d404
Tomasz Kamiński <tkaminski93@gmail.com>
2016-03-26 18:45:59 +0100

    sdfsdfsd

---
+++ src/ParkingATHWeb.Contracts/DTO/UserPreferences/UserPreferencesDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using ParkingATHWeb.Contracts.Common;
+
+namespace ParkingATHWeb.Contracts.DTO.UserPreferences
+{
+    public class UserPreferencesDto : BaseDto<int>
+    {
+        public bool ShrinkedSidebar { get; set; }
+        public byte[] ProfilePhoto { get; set; }
+        public Guid? ProfilePhotoId { get; set; }
+
+        public int UserId { get; set; }
+    }
+}
